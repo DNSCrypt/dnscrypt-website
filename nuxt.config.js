c72ab20571db81d794e6b9c8f190fdd7d5f1aaf8
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-02-15 13:53:23 +1000

    add missing plugins/vuetify.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 module.exports = {
   mode: "universal",
-  plugins: ["~plugins/vuetify.js"],
+  plugins: ["~/plugins/vuetify.js"],
   modules: ["@nuxtjs/markdownit"],
 
   css: [
---
+++ plugins/vuetify.js
@@ -0,0 +1,36 @@
+import Vue from 'vue'
+import {
+  Vuetify,
+  VApp,
+  VCard,
+  VNavigationDrawer,
+  VFooter,
+  VList,
+  VBtn,
+  VIcon,
+  VGrid,
+  VToolbar
+} from 'vuetify'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#9c27b0',
+    accent: '#ce93d8',
+    secondary: '#424242',
+    info: '#0D47A1',
+    warning: '#ffb300',
+    error: '#B71C1C',
+    success: '#2E7D32'
+  },
+  components: {
+    VApp,
+    VCard,
+    VNavigationDrawer,
+    VFooter,
+    VList,
+    VBtn,
+    VIcon,
+    VGrid,
+    VToolbar
+  }
+})
