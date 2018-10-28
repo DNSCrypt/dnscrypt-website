8f737c4848ef26d39f810d42e73a7152df8890e1
Sebastian Schmidt <publicarray@users.noreply.github.com>
2018-10-28 11:31:31 +1100

    Materialize implementation tables

--- pages/implementations.vue
+++ pages/implementations.vue
@@ -1,56 +1,214 @@
-<template lang=md>
+<template>
+  <div>
+    <h2>Client Implementations</h2>
+    <v-card>
+      <v-card-title>
+        Client Implementations
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :disable-initial-sort="true" :headers="clientHeaders" :items="clientImplementations" :search="search" hide-actions class="elevation-1">
+        <template slot="items" slot-scope="props">
+            <tr :title="props.item.description">
+              <td>
+                <a v-if="props.item.url" :href="props.item.url">{{ props.item.name }}</a>
+                <span v-else>{{ props.item.name }}</span>
+              </td>
+              <td>{{ props.item.Author }}</td>
+              <td>
+                <span v-for="(protocol, index) in props.item.protocols" :key="index">
+                  {{ protocol }}<span v-if="index == props.item.protocols.length-2"> and </span><span v-if="index < props.item.protocols.length-2">, </span>
+                </span>
+              </td>
+              <td>
+                <span v-for="(platform, index) in props.item.platforms" :key="index">
+                  {{ platform }}<span v-if="index == props.item.platforms.length-2"> and </span><span v-if="index < props.item.platforms.length-2">, </span>
+                </span>
+              </td>
+              <td>{{ props.item.language }}</td>
+            </tr>
+        </template>
+      </v-data-table>
+    </v-card>
 
-# Client Implementations
+    <h2>Server Implementations</h2>
+    <v-card>
+      <v-card-title>
+        Server Implementations
+        <v-spacer></v-spacer>
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="serverHeaders" :items="serverImplementations" :search="search" hide-actions class="elevation-1">
+        <template slot="items" slot-scope="props">
+            <tr :title="props.item.description">
+              <td><a :href="props.item.url">{{ props.item.name }}</a></td>
+              <td>{{ props.item.author }}</td>
+              <td>
+                <span v-for="(protocol, index) in props.item.protocols" :key="index">
+                  {{ protocol }}<span v-if="index == props.item.protocols.length-2"> and </span><span v-if="index < props.item.protocols.length-2">, </span>
+                </span>
+              </td>
+              <td>{{ props.item.language }}</td>
+            </tr>
+        </template>
+      </v-data-table>
+    </v-card>
 
-In order to use DNS over secure channels, client software has to be installed on your devices, or on your router.
-
-### All Platforms
-
-* [DNSCrypt-Proxy](https://github.com/jedisct1/dnscrypt-proxy) is a command-line proxy for Linux, BSD, Windows, MacOS, Android and more. It bridges applications expecting regular DNS with servers supporting encrypted DNS (DNSCrypt and DoH). DNSCrypt-Proxy can also display the DNS activity, cache results to improve speed, and locally block unwanted content. [Installation instructions](https://github.com/jedisct1/dnscrypt-proxy/wiki/installation) - [Download](https://github.com/jedisct1/dnscrypt-proxy/releases)
-
-* [DoH-proxy](https://facebookexperimental.github.io/doh-proxy/) by Facebook, supports DNS-over-HTTP/2 (DoH)
-
-* [Pcap_DNSProxy](https://github.com/chengr28/Pcap_DNSProxy) is a local DNS proxy based on WinPcap and LibPcap
-
-* [YourFriendlyDNS](https://github.com/softwareengineer1/YourFriendlyDNS) is a local, multi-platform (Linux, Windows, macOS, Android) caching DNS proxy written in C++
-
-### Windows
-
-* [Simple DNSCrypt](https://simplednscrypt.org/) is a simple management tool to configure dnscrypt-proxy on windows based systems.
-
-### macOS
-
-* [dnscrypt-proxy switcher](https://github.com/jedisct1/bitbar-dnscrypt-proxy-switcher/) plugin for [BitBar](https://getbitbar.com).
-
-### iOS
-
-* [DNSCloak](https://itunes.apple.com/us/app/dnscloak-dnscrypt-client/id1330471557?mt=8): full-featured DNSCrypt client for iOS, with filtering, logging, caching, password protection and more. No jailbreak required.
-
-### Android
-
-Official pre-compiled binaries are available for Android. A Magisk module is also available. See this thread on running [DNSCrypt proxy on Android](https://github.com/jedisct1/dnscrypt-proxy/issues/41) for more information.
-
-### Other platforms
-
-Be a hero and develop free, open-source clients for your favourite platform!
-
-# Server Implementations
-
-* [DNSCrypt-Wrapper](https://github.com/cofyc/dnscrypt-wrapper)
-  * Pre-build [Docker container](https://github.com/jedisct1/dnscrypt-server-docker) uses Unbound for DNS resolution and DNSCrypt-Wrapper to proxy dnscrypt.
-
-* [Unbound](https://unbound.net) by NLnetLabs, supports both DNS-over-TLS and DNSCrypt.
-
-* [dnsdist](https://dnsdist.org) by PowerDNS, supports both DNS-over-TLS and DNSCrypt.
-
-* [DoH-proxy](https://facebookexperimental.github.io/doh-proxy/) by Facebook, supports DNS-over-HTTP/2 (DoH)
-
-* [rust-DoH](https://github.com/jedisct1/rust-doh) supports DNS-over-HTTP/2 (DoH)
-
-## Server Setup Guides
-
-* [How to setup your own DNSCrypt server in less than 10 minutes](https://github.com/jedisct1/dnscrypt-proxy/wiki/How-to-setup-your-own-DNSCrypt-server-in-less-than-10-minutes)
-
-* [How to setup your own DNSCrypt server on Vultr](https://github.com/jedisct1/dnscrypt-proxy/wiki/DNSCrypt-server-with-vultr.com)
-
+    <h2>Server Setup Guides</h2>
+    <p><a href="https://github.com/jedisct1/dnscrypt-proxy/wiki/How-to-setup-your-own-DNSCrypt-server-in-less-than-10-minutes">How to setup your own DNSCrypt server in less than 10 minutes</a></p>
+    <p><a href="https://github.com/jedisct1/dnscrypt-proxy/wiki/DNSCrypt-server-with-vultr.com">How to setup your own DNSCrypt server on Vultr</a></p>
+  </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+        search: '',
+        clientHeaders: [
+          { text: 'Name', value: 'name' },
+          { text: 'Author', value: 'Author' },
+          { text: 'Protocols', value: 'protocols' },
+          { text: 'Platforms', value: 'platforms' },
+          { text: 'Language', value: 'language' },
+        ], clientImplementations: [
+          {
+            name: "DNSCrypt-Proxy",
+            Author: "Frank Denis (@jedisct1)",
+            url: "https://github.com/jedisct1/dnscrypt-proxy",
+            protocols: ["DNSProxy", "DoH"],
+            platforms: ["Linux", "BSD", "Windows", "macOS", "Android", "more"],
+            language: "Golang",
+            description: "DNSCrypt-Proxy is a command-line proxy for Linux, BSD, Windows, MacOS, Android and more. It bridges applications expecting regular DNS with servers supporting encrypted DNS (DNSCrypt and DoH). DNSCrypt-Proxy can also display the DNS activity, cache results to improve speed, and locally block unwanted content."
+          }, {
+            name: "DoH-proxy",
+            Author: "Facebook",
+            url: "https://facebookexperimental.github.io/doh-proxy/",
+            protocols: ["DoH"],
+            platforms: ["Linux", "BSD", "Windows", "macOS", "more"],
+            language: "Python",
+            description: "DoH-proxy by Facebook, supports DNS-over-HTTP/2 (DoH)"
+          }, {
+            name: "Pcap_DNSProxy",
+            Author: "",
+            url: "https://github.com/chengr28/Pcap_DNSProxy",
+            protocols: ["DNSProxy"],
+            platforms: ["Windows", "Linux", "macOS", "OpenWrt/LEDE"],
+            language: "C++",
+            description: "Pcap_DNSProxy is a local DNS proxy based on WinPcap and LibPcap"
+          }, {
+            name: "YourFriendlyDNS",
+            Author: "",
+            url: "https://github.com/softwareengineer1/YourFriendlyDNS",
+            protocols: ["DNSProxy"],
+            platforms: ["Linux", "Windows", "macOS", "Android"],
+            language: "C++",
+            description: "YourFriendlyDNS is a local, multi-platform (Linux, Windows, macOS, Android) caching DNS proxy written in C++"
+          }, {
+            name: "Simple DNSCrypt",
+            Author: "",
+            url: "https://simplednscrypt.org/",
+            protocols: ["DNSProxy"],
+            platforms: ["Windows"],
+            language: "C#",
+            description: "Simple DNSCrypt is a simple management tool to configure dnscrypt-proxy on windows based systems."
+          }, {
+            name: "dnscrypt-proxy switcher",
+            Author: "",
+            url: "https://github.com/jedisct1/bitbar-dnscrypt-proxy-switcher/",
+            protocols: ["DNSProxy"],
+            platforms: ["macOS"],
+            language: "Shell",
+            description: "dnscrypt-proxy switcher is a plugin for BitBar."
+          }, {
+            name: "DNSCloak",
+            Author: "",
+            url: "https://itunes.apple.com/us/app/dnscloak-dnscrypt-client/id1330471557?mt=8",
+            protocols: ["DNSProxy"],
+            platforms: ["iOS"],
+            language: "",
+            description: "DNSCloak is a full-featured DNSCrypt client for iOS, with filtering, logging, caching, password protection and more. No jailbreak required."
+          }, {
+            name: "DNSCrypt proxy on Android",
+            Author: "",
+            url: "https://github.com/jedisct1/dnscrypt-proxy/issues/41",
+            protocols: ["DNSProxy"],
+            platforms: ["Android"],
+            language: "",
+            description: "Official pre-compiled binaries are available for Android. A Magisk module is also available. See thread on running DNSCrypt proxy on Android for more information."
+          }, {
+            name: "Other platforms",
+            Author: "",
+            url: "",
+            protocols: [],
+            platforms: [],
+            language: "",
+            description: "Be a hero and develop free, open-source clients for your favourite platform!"
+          }
+        ],
+        serverHeaders: [
+          { text: 'Name', value: 'name' },
+          { text: 'Author', value: 'author' },
+          { text: 'Protocols', value: 'protocols' },
+          { text: 'Language', value: 'language' },
+        ], serverImplementations: [
+          {
+            name: "DNSCrypt-Wrapper",
+            author: "Yecheng Fu (@cofyc)",
+            url: "https://github.com/cofyc/dnscrypt-wrapper",
+            protocols: [ "DNSCrypt", "DoT" ],
+            language: "C",
+            description: "DNSCrypt-Wrapper is a DNSCrypt proxy."
+          },{
+            name: "Official Docker container (DNSCrypt-Wrapper and Unbound)",
+            author: "",
+            url: "https://github.com/DNSCrypt/dnscrypt-server-docker",
+            protocols: [ "DNSCrypt" ],
+            language: "",
+            description: ""
+          }, {
+            name: "Unbound",
+            author: "NLnet Labs",
+            url: "https://nlnetlabs.nl/projects/unbound/about/",
+            protocols: [ "DNSCrypt" ],
+            language: "C",
+            description: "Unbound by NLnetLabs, supports both DNS-over-TLS and DNSCrypt."
+          }, {
+            name: "dnsdist",
+            author: "PowerDNS",
+            url: "https://dnsdist.org/",
+            protocols: [ "DNSCrypt" ],
+            language: "C++",
+            description: "dnsdist by PowerDNS, supports both DNS-over-TLS and DNSCrypt."
+          }, {
+            name: "DoH-proxy",
+            author: "Facebook",
+            url: "https://facebookexperimental.github.io/doh-proxy/",
+            protocols: [ "DoH" ],
+            language: "Python",
+            description: "DoH-proxy by Facebook, supports DNS-over-HTTP/2 (DoH)"
+          }, {
+            name: "Rust-DoH",
+            author: "Frank Denis (@jedisct1)",
+            url: "https://github.com/jedisct1/rust-doh",
+            protocols: [ "DoH" ],
+            language: "Rust",
+            description: "rust-DoH supports DNS-over-HTTP/2 (DoH)"
+          }
+        ]
+      }
+    }
+  }
+</script>
