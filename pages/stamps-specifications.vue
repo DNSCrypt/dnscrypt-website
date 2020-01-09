48004a2315eb27c332a4f0c19a16e74b3883af51
Frank Denis <github@pureftpd.org>
2020-01-09 11:44:53 -0700

    Add missing quotes

--- pages/stamps-specifications.vue
+++ pages/stamps-specifications.vue
@@ -122,7 +122,7 @@ Format:
 
 0x81 is the protocol identifier for a DNSCrypt relay.
 
-addr is the IP address and port, as a string. IPv6 strings must be included in square brackets: [fe80::6d6d:f72c:3ad:60b8]:443.
+addr is the IP address and port, as a string. IPv6 strings must be included in square brackets: `[fe80::6d6d:f72c:3ad:60b8]:443`.
 
 ## Implementations
 
