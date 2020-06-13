a2394442c3eefdc5ae9bf4d2c9ce8b09af2667e1
Frank Denis <github@pureftpd.org>
2020-06-13 19:05:01 +0200

    Rename some navigation items

--- components/TopMenu.vue
+++ components/TopMenu.vue
@@ -12,10 +12,10 @@
           <v-list-tile-title>Encrypted DNS</v-list-tile-title>
         </v-list-tile>
         <v-list-tile nuxt to="/implementations">
-          <v-list-tile-title>Implementations</v-list-tile-title>
+          <v-list-tile-title>Software</v-list-tile-title>
         </v-list-tile>
         <v-list-tile nuxt to="/public-servers">
-          <v-list-tile-title>Encrypted DNS servers</v-list-tile-title>
+          <v-list-tile-title>DNSCrypt &amp; DoH servers</v-list-tile-title>
         </v-list-tile>
         <v-list-tile nuxt to="/map">
           <v-list-tile-title>Map</v-list-tile-title>
@@ -27,18 +27,18 @@
           <v-list-tile-title>Protocol</v-list-tile-title>
         </v-list-tile>
         <v-list-tile nuxt to="/stamps">
-          <v-list-tile-title>Stamps</v-list-tile-title>
+          <v-list-tile-title>DNS Stamps</v-list-tile-title>
         </v-list-tile>
       </v-list>
       <v-toolbar-side-icon slot="activator" class="hidden-md-and-up"></v-toolbar-side-icon>
     </v-menu>
     <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat nuxt to="/implementations">Implementations</v-btn>
-      <v-btn flat nuxt to="/public-servers">Encrypted DNS servers</v-btn>
+      <v-btn flat nuxt to="/implementations">Software</v-btn>
+      <v-btn flat nuxt to="/public-servers">DNSCrypt &amp; DoH servers</v-btn>
       <v-btn flat nuxt to="/map">Map</v-btn>
       <v-btn flat nuxt to="/faq">FAQ</v-btn>
       <v-btn flat nuxt to="/protocol">Protocol</v-btn>
-      <v-btn flat nuxt to="/stamps">Stamps</v-btn>
+      <v-btn flat nuxt to="/stamps">DNS Stamps</v-btn>
     </v-toolbar-items>
     <v-btn
       flat
