7a535304236e16a3fb461b72362104961da8f663
Frank Denis <github@pureftpd.org>
2020-07-05 15:22:53 +0200

    Missing space

--- pages/stamps.vue
+++ pages/stamps.vue
@@ -2,7 +2,7 @@
   <v-container>
     <DNSStamp />
     <p>
-      <nuxt-link to="/stamps-specifications">DNS Stamps</nuxt-link>encode all the parameters required to connect to a secure DNS server as a single string.
+      <nuxt-link to="/stamps-specifications">DNS Stamps</nuxt-link> encode all the parameters required to connect to a secure DNS server as a single string.
     </p>
     <p>Think about stamps as QR code, but for DNS.</p>
     <p>Useful links for DNS operators:</p>
