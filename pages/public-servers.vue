b4861f1a1ef6101d3b23124acfcc693804cdb2b4
Frank Denis <github@pureftpd.org>
2021-01-15 00:20:19 +0100

    Add a link to the resolvers repository

--- pages/public-servers.vue
+++ pages/public-servers.vue
@@ -1,79 +1,93 @@
 <template>
-  <v-data-table
-    :headers="headers"
-    :items="items"
-    :loading="loading"
-    class="elevation-1"
-    :rows-per-page-items="[50, 25, 10, { text: 'All', value: -1 }]"
-  >
-    <template slot="items" slot-scope="props">
-      <td>
-        <v-dialog v-model="dialog" width="50%">
-          <template v-slot:activator="{ on }">
-            <v-badge>
-              <span v-on="on" :style="{ cursor: 'pointer' }">{{
+  <v-container>
+    <v-data-table
+      :headers="headers"
+      :items="items"
+      :loading="loading"
+      class="elevation-1"
+      :rows-per-page-items="[50, 25, 10, { text: 'All', value: -1 }]"
+    >
+      <template slot="items" slot-scope="props">
+        <td>
+          <v-dialog v-model="dialog" width="50%">
+            <template v-slot:activator="{ on }">
+              <v-badge>
+                <span v-on="on" :style="{ cursor: 'pointer' }">{{
+                  props.item.name
+                }}</span>
+                <span slot="badge" title="IPv6" v-if="props.item.ipv6">v6</span>
+              </v-badge>
+            </template>
+            <v-card>
+              <v-card-title class="headline grey lighten-2" primary-title>{{
                 props.item.name
-              }}</span>
-              <span slot="badge" title="IPv6" v-if="props.item.ipv6">v6</span>
-            </v-badge>
-          </template>
-          <v-card>
-            <v-card-title class="headline grey lighten-2" primary-title>{{
-              props.item.name
-            }}</v-card-title>
-            <v-card-text>{{ props.item.description }}</v-card-text>
-            <v-card-text>
-              <v-simple-table>
-                <tbody>
-                  <tr>
-                    <th class="text-right">Protocol</th>
-                    <td>{{ props.item.proto }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">Addresses</th>
-                    <td>{{ props.item.addrs }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">Ports</th>
-                    <td>{{ props.item.ports }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">DNSSEC</th>
-                    <td>{{ props.item.dnssec }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">No filters</th>
-                    <td>{{ props.item.nofilter }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">No logs</th>
-                    <td>{{ props.item.nolog }}</td>
-                  </tr>
-                  <tr>
-                    <th class="text-right">Stamp</th>
-                    <td>{{ props.item.stamp }}</td>
-                  </tr>
-                </tbody>
-              </v-simple-table>
-            </v-card-text>
-          </v-card>
-        </v-dialog>
-      </td>
-      <td class="hidden-xs-only">
-        {{ props.item.description }}
-        <v-icon title="Filtering" color="pink" small v-if="!props.item.nofilter"
-          >block</v-icon
-        >
-      </td>
-      <td class="hidden-sm-and-down" align="center">{{ props.item.proto }}</td>
-      <td align="center">
-        <v-icon color="red" v-if="!props.item.nolog">warning</v-icon>
-      </td>
-      <td class="hidden-sm-and-down" align="center">
-        <v-icon color="blue" v-if="props.item.dnssec">lock</v-icon>
-      </td>
-    </template>
-  </v-data-table>
+              }}</v-card-title>
+              <v-card-text>{{ props.item.description }}</v-card-text>
+              <v-card-text>
+                <v-simple-table>
+                  <tbody>
+                    <tr>
+                      <th class="text-right">Protocol</th>
+                      <td>{{ props.item.proto }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">Addresses</th>
+                      <td>{{ props.item.addrs }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">Ports</th>
+                      <td>{{ props.item.ports }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">DNSSEC</th>
+                      <td>{{ props.item.dnssec }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">No filters</th>
+                      <td>{{ props.item.nofilter }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">No logs</th>
+                      <td>{{ props.item.nolog }}</td>
+                    </tr>
+                    <tr>
+                      <th class="text-right">Stamp</th>
+                      <td>{{ props.item.stamp }}</td>
+                    </tr>
+                  </tbody>
+                </v-simple-table>
+              </v-card-text>
+            </v-card>
+          </v-dialog>
+        </td>
+        <td class="hidden-xs-only">
+          {{ props.item.description }}
+          <v-icon
+            title="Filtering"
+            color="pink"
+            small
+            v-if="!props.item.nofilter"
+            >block</v-icon
+          >
+        </td>
+        <td class="hidden-sm-and-down" align="center">
+          {{ props.item.proto }}
+        </td>
+        <td align="center">
+          <v-icon color="red" v-if="!props.item.nolog">warning</v-icon>
+        </td>
+        <td class="hidden-sm-and-down" align="center">
+          <v-icon color="blue" v-if="props.item.dnssec">lock</v-icon>
+        </td>
+      </template>
+    </v-data-table>
+    <p class="text-center headline">
+      Click on a server name to view details -
+      <a href="https://github.com/dnscrypt/dnscrypt-resolvers"
+        >This server list is maintained on GitHub</a
+      >
+    </p>
+  </v-container>
 </template>
 
 <script>
