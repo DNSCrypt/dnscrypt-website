e4d2463c0edff1bc79cddfbddf1c930a332fcfb6
Frank Denis <github@pureftpd.org>
2023-03-02 19:24:48 +0100

    Add DNS over Oblivious HTTP stamps

--- assets/Stamps.md
+++ assets/Stamps.md
@@ -141,6 +141,19 @@ The port number is optional, and is assumed to be `443` if missing.
 
 `path` is the absolute URI path, such as `/dns-query`.
 
+## DNS over Oblivious HTTP target stamps
+
+Format:
+
+```text
+"sdns://" || base64url(0x06 || props || LP(hostname [:port]) || LP(path))
+```
+
+`hostname` is the server host name which, for relays, will also be used as a SNI name. If the host name contains characters outside the URL-permitted range, these characters should be sent as-is, without any extra encoding (neither URL-encoded nor punycode).
+The port number is optional, and is assumed to be `443` if missing.
+
+`path` is the absolute URI path, such as `/dns-query`.
+
 ## Anonymized DNSCrypt relay stamps
 
 Format:
@@ -164,6 +177,32 @@ Format:
 ```
 
 `0x85` is the protocol identifier for an oDoH relay.
+
+`addr` is the IP address of the server.
+It can be an empty string. In that case, the host name will be resolved to an IP address using another resolver.
+
+`hashi` is the SHA256 digest of one of the TBS certificate found in the validation chain,
+typically the certificate used to sign the resolver's certificate.  Multiple hashes can
+be provided for seamless rotations.
+
+`hostname` is the server host name which, for relays, will also be used as a SNI name. If the host name contains characters outside the URL-permitted range, these characters should be sent as-is, without any extra encoding (neither URL-encoded nor punycode).
+The port number is optional, and is assumed to be `443` if missing.
+
+`path` is the absolute URI path, such as `/dns-query`.
+
+`bootstrap_ipi` are IP addresses of recommended resolvers accessible over standard DNS in order to resolve `hostname` and get the oDoH target public key. This is optional, and clients can ignore this information.
+
+## DNS over Oblivious HTTP relay stamps
+
+Format:
+
+```text
+"sdns://" || base64url(0x86 || props || LP(addr) || VLP(hash1, hash2, ...hashn) ||
+                       LP(hostname [:port]) || LP(path)
+                       [ || VLP(bootstrap_ip1, bootstrap_ip2, ...bootstrap_ipn) ])
+```
+
+`0x86` is the protocol identifier for an oDoH relay.
 
 `addr` is the IP address of the server.
 It can be an empty string. In that case, the host name will be resolved to an IP address using another resolver.
--- components/DNSStamp.vue
+++ components/DNSStamp.vue
@@ -3,91 +3,65 @@
     <h1>Online DNS Stamp calculator</h1>
     <v-layout row wrap>
       <v-flex xs12 sm2>
-        <v-select
-          label="Protocol"
-          :items="[
-            { text: 'DNSCrypt', value: 'DNSCrypt' },
-            { text: 'DNS-over-HTTPS (DoH)', value: 'DoH' },
-            { text: 'Anonymized DNS', value: 'DNSCryptRelay' },
-            { text: 'DNS-over-TLS', value: 'DoT' },
-            { text: 'DNS-over-QUIC', value: 'DoQ' },
-            { text: 'Oblivious DoH target', value: 'oDoHTarget' },
-            { text: 'Oblivious DoH relay', value: 'oDoHRelay' },
-            { text: 'Plain DNS', value: 'PlainDNS' },
-          ]"
-          v-model="proto"
-        />
-        <span v-if="proto !== 'DNSCryptRelay' && proto !== 'oDoHRelay'">
-          <v-checkbox label="DNSSEC" v-model="dnssec" />
-          <v-checkbox label="No filter" v-model="nofilter" />
-        </span>
-        <span v-if="proto !== 'DNSCryptRelay'">
-          <v-checkbox label="No logs" v-model="nolog" />
-        </span>
-      </v-flex>
-      <v-flex xs12 sm6>
-        <span v-if="proto !== 'oDoHTarget'">
-          <v-text-field
-            label="IP Address (IPv6 addresses must be in [ ] brackets)"
-            type="text"
-            v-model="addr"
-          />
-        </span>
-        <span v-if="proto === 'DNSCrypt'">
-          <v-text-field label="Provider public key" type="text" v-model="pk" />
-          <v-text-field
-            label="Provider name"
-            type="text"
-            v-model="providerName"
-          />
-        </span>
-        <span
-          v-if="
-            proto === 'DoH' ||
-            proto === 'DoT' ||
-            proto === 'DoQ' ||
-            proto === 'oDoHTarget' ||
-            proto === 'oDoHRelay'
-          "
-        >
-          <v-text-field
-            label="Host name (vhost+SNI) and optional port number"
-            type="text"
-            v-model="hostName"
-          />
-        </span>
-        <span
-          v-if="
-            proto === 'DoH' ||
-            proto === 'DoT' ||
-            proto === 'DoQ' ||
-            proto === 'oDoHRelay'
-          "
-        >
-          <v-text-field
-            label="Hashes (comma-separated)"
-            type="text"
-            v-model="hashes"
-          />
-        </span>
-        <span
-          v-if="
-            proto === 'DoH' || proto === 'oDoHRelay' || proto === 'oDoHTarget'
-          "
-        >
-          <v-text-field label="Path" type="text" v-model="path" />
-        </span>
-      </v-flex>
-      <v-flex xs12 sm4>
-        <v-text-field
-          label="Stamp"
-          type="text"
-          :value="stamp"
-          @input="stampUpdated"
-        />
-      </v-flex>
-    </v-layout>
-  </v-container>
+              <v-select label="Protocol" :items="[
+                {text: 'DNSCrypt', value: 'DNSCrypt'},
+                {text: 'DNS-over-HTTPS (DoH)', value: 'DoH'},
+                {text: 'Anonymized DNS', value: 'DNSCryptRelay'},
+                {text: 'DNS-over-TLS', value: 'DoT'},
+                {text: 'DNS-over-QUIC', value: 'DoQ'},
+                {text: 'Oblivious DoH target', value: 'oDoHTarget'},
+                {text: 'Oblivious DoH relay', value: 'oDoHRelay'},
+                {text: 'DNS over Oblivious HTTP target', value: 'DoOHTarget'},
+                {text: 'DNS over Oblivious HTTP relay', value: 'DoOHRelay'},
+                {text: 'Plain DNS', value: 'PlainDNS'},
+              ]" v-model="proto" />
+              <span v-if="proto !== 'DNSCryptRelay' && proto !== 'oDoHRelay'">
+                <v-checkbox label="DNSSEC" v-model="dnssec" />
+                <v-checkbox label="No filter" v-model="nofilter" />
+              </span>
+              <span v-if="proto !== 'DNSCryptRelay'">
+                <v-checkbox label="No logs" v-model="nolog" />
+              </span>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <span v-if="proto !== 'oDoHTarget' && proto !== 'DoOHTarget'">
+                <v-text-field label="IP Address (IPv6 addresses must be in [ ] brackets)" type="text" v-model="addr" />
+              </span>
+              <span v-if="proto === 'DNSCrypt'">
+                <v-text-field label="Provider public key" type="text" v-model="pk" />
+                <v-text-field label="Provider name" type="text" v-model="providerName" />
+              </span>
+              <span v-if="
+                proto === 'DoH' ||
+                proto === 'DoT' ||
+                proto === 'DoQ' ||
+                proto === 'oDoHTarget' ||
+                proto === 'oDoHRelay' ||
+                proto === 'DoOHTarget' ||
+                proto === 'DoOHRelay'
+              ">
+                <v-text-field label="Host name (vhost+SNI) and optional port number" type="text" v-model="hostName" />
+              </span>
+              <span v-if="
+                proto === 'DoH' ||
+                proto === 'DoT' ||
+                proto === 'DoQ' ||
+                proto === 'oDoHRelay' ||
+                proto === 'DoOHRelay'
+              ">
+                <v-text-field label="Hashes (comma-separated)" type="text" v-model="hashes" />
+              </span>
+              <span v-if="
+                proto === 'DoH' || proto === 'oDoHRelay' || proto === 'oDoHTarget' || proto === 'DoOHRelay' || proto === 'DoOHTarget'
+              ">
+                <v-text-field label="Path" type="text" v-model="path" />
+              </span>
+            </v-flex>
+            <v-flex xs12 sm4>
+              <v-text-field label="Stamp" type="text" :value="stamp" @input="stampUpdated" />
+            </v-flex>
+          </v-layout>
+        </v-container>
 </template>
 
 <script>
@@ -141,10 +115,14 @@ export default {
         this.proto = "DoQ";
       } else if (bin[0] === 0x05) {
         this.proto = "oDoHTarget";
+      } else if (bin[0] === 0x06) {
+        this.proto = "DoOHTarget";
       } else if (bin[0] === 0x81) {
         this.proto = "DNSCryptRelay";
       } else if (bin[0] === 0x85) {
         this.proto = "oDoHRelay";
+      } else if (bin[0] === 0x86) {
+        this.proto = "DoOHRelay";
       } else {
         return;
       }
@@ -158,7 +136,7 @@ export default {
         i += 8;
       }
 
-      if (this.proto !== "oDoHTarget") {
+      if (this.proto !== "oDoHTarget" && this.proto !== "DoOHTarget") {
         let addrLen = bin[i++];
         this.addr = bin.slice(i, i + addrLen).toString("utf-8");
         i += addrLen;
@@ -174,7 +152,7 @@ export default {
 
       const dohStamp = () => {
         this.hashes = "";
-        for (;;) {
+        for (; ;) {
           let hashLen = bin[i++];
           this.hashes += bin.slice(i, i + (hashLen & 0x7f)).toString("hex");
           i += hashLen & 0x7f;
@@ -193,7 +171,7 @@ export default {
 
       const dotStamp = () => {
         this.hashes = "";
-        for (;;) {
+        for (; ;) {
           let hashLen = bin[i++];
           this.hashes += bin.slice(i, i + (hashLen & 0x7f)).toString("hex");
           i += hashLen & 0x7f;
@@ -209,7 +187,7 @@ export default {
 
       const doqStamp = () => {
         this.hashes = "";
-        for (;;) {
+        for (; ;) {
           let hashLen = bin[i++];
           this.hashes += bin.slice(i, i + (hashLen & 0x7f)).toString("hex");
           i += hashLen & 0x7f;
@@ -231,13 +209,40 @@ export default {
         this.path = bin.slice(i, i + pathLen).toString("utf-8");
       };
 
+      const doohTargetStamp = () => {
+        let hostNameLen = bin[i++];
+        this.hostName = bin.slice(i, i + hostNameLen).toString("utf-8");
+        i += hostNameLen;
+        let pathLen = bin[i++];
+        this.path = bin.slice(i, i + pathLen).toString("utf-8");
+      };
+
       const dnscryptRelayStamp = () => {
         // DNS relays only include IP addresses
       };
 
       const odohRelayStamp = () => {
         this.hashes = "";
-        for (;;) {
+        for (; ;) {
+          let hashLen = bin[i++];
+          this.hashes += bin.slice(i, i + (hashLen & 0x7f)).toString("hex");
+          i += hashLen & 0x7f;
+          if ((hashLen & 0x80) == 0x80) {
+            this.hashes += ",";
+          } else {
+            break;
+          }
+        }
+        let hostNameLen = bin[i++];
+        this.hostName = bin.slice(i, i + hostNameLen).toString("utf-8");
+        i += hostNameLen;
+        let pathLen = bin[i++];
+        this.path = bin.slice(i, i + pathLen).toString("utf-8");
+      };
+
+      const doohRelayStamp = () => {
+        this.hashes = "";
+        for (; ;) {
           let hashLen = bin[i++];
           this.hashes += bin.slice(i, i + (hashLen & 0x7f)).toString("hex");
           i += hashLen & 0x7f;
@@ -264,10 +269,14 @@ export default {
         doqStamp();
       } else if (this.proto === "oDoHTarget") {
         odohTargetStamp();
+      } else if (this.proto === "DoOHTarget") {
+        doohTargetStamp();
       } else if (this.proto === "DNSCryptRelay") {
         dnscryptRelayStamp();
       } else if (this.proto === "oDoHRelay") {
         odohRelayStamp();
+      } else if (this.proto === "DoOHRelay") {
+        doohRelayStamp();
       }
     },
   },
@@ -297,7 +306,7 @@ export default {
           hashes = this.hashes
             .split(/ *, */)
             .map((h) => Buffer.from(h.replace(/[: \t]/g, ""), "hex"));
-        } catch (e) {}
+        } catch (e) { }
         if (hashes.length === 0) {
           v.push(0);
         } else {
@@ -324,7 +333,7 @@ export default {
           hashes = this.hashes
             .split(/ *, */)
             .map((h) => Buffer.from(h.replace(/[: \t]/g, ""), "hex"));
-        } catch (e) {}
+        } catch (e) { }
         if (hashes.length === 0) {
           v.push(0);
         } else {
@@ -349,7 +358,7 @@ export default {
           hashes = this.hashes
             .split(/ *, */)
             .map((h) => Buffer.from(h.replace(/[: \t]/g, ""), "hex"));
-        } catch (e) {}
+        } catch (e) { }
         if (hashes.length === 0) {
           v.push(0);
         } else {
@@ -375,6 +384,15 @@ export default {
         return `sdns://${URLSafeBase64.encode(Buffer(v))}`;
       };
 
+      const doohTargetStamp = () => {
+        let v = [0x06, props, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        let hostName = this.hostName.split("").map((c) => c.charCodeAt());
+        v.push(hostName.length, ...hostName);
+        let path = this.path.split("").map((c) => c.charCodeAt());
+        v.push(path.length, ...path);
+        return `sdns://${URLSafeBase64.encode(Buffer(v))}`;
+      };
+
       const dnscryptRelayStamp = () => {
         let v = [0x81];
         v.push(addr.length, ...addr);
@@ -389,7 +407,34 @@ export default {
           hashes = this.hashes
             .split(/ *, */)
             .map((h) => Buffer.from(h.replace(/[: \t]/g, ""), "hex"));
-        } catch (e) {}
+        } catch (e) { }
+        if (hashes.length === 0) {
+          v.push(0);
+        } else {
+          for (let i = 0, j = hashes.length; i < j; i++) {
+            let length = hashes[i].length;
+            if (i < j - 1) {
+              length |= 0x80;
+            }
+            v.push(length, ...hashes[i]);
+          }
+        }
+        let hostName = this.hostName.split("").map((c) => c.charCodeAt());
+        v.push(hostName.length, ...hostName);
+        let path = this.path.split("").map((c) => c.charCodeAt());
+        v.push(path.length, ...path);
+        return `sdns://${URLSafeBase64.encode(Buffer(v))}`;
+      };
+
+      const doohRelayStamp = () => {
+        let v = [0x86, props, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00];
+        v.push(addr.length, ...addr);
+        let hashes = [];
+        try {
+          hashes = this.hashes
+            .split(/ *, */)
+            .map((h) => Buffer.from(h.replace(/[: \t]/g, ""), "hex"));
+        } catch (e) { }
         if (hashes.length === 0) {
           v.push(0);
         } else {
@@ -426,10 +471,14 @@ export default {
         return doqStamp();
       } else if (this.proto === "oDoHTarget") {
         return odohTargetStamp();
+      } else if (this.proto === "DoOHTarget") {
+        return doohTargetStamp();
       } else if (this.proto === "DNSCryptRelay") {
         return dnscryptRelayStamp();
       } else if (this.proto === "oDoHRelay") {
         return odohRelayStamp();
+      } else if (this.proto === "DoOHRelay") {
+        return doohRelayStamp();
       } else {
         return dnscryptStamp();
       }
